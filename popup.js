3a5dfae7a622d6cfe8a7e9624adc9638c7238030
dohyunk4 <dohyun.k4@gmail.com>
2015-11-26 09:44:57 -0500

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,74 @@
+function setLikesInfo()
+{
+    console.log("working!"); 
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+    // send message to contentscript in selected tab
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    },
+        function(tabs)
+        {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {from: 'popup', subject: 'Likes'},
+                setLikesInfo
+                );
+        });
+});
+
+
+
+
+/*function main() {
+    // Initialization work goes here.
+    chrome.tabs.executeScript(null, {
+        file: "content.js"
+    });
+    chrome.tabs.executeScript(null, {
+        code: "javascript:likeAll(likes)"
+    });
+    
+ 
+    function likeItAll() {
+        chrome.tabs.executeScript(null, {
+            file: "content.js"
+        });
+        chrome.tabs.executeScript(null, {
+            code: "javascript:likeAll(likes)"
+        });
+        onLoad = null;
+    }
+ 
+}
+ 
+function linkToTwitter() {
+    chrome.tabs.create({
+        url: "http://twitter.com/danest"
+    });
+}
+ 
+function linktoNineGraphs() {
+    chrome.tabs.create({
+        url: "http://www.9graphs.com/"
+    });
+}
+ 
+function linktoTwiterFavorite() {
+    chrome.tabs.create({
+        url: "http://www.twitterfavorite.com/"
+    });
+}
+ 
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('likeAllDanestTwitter')
+        .addEventListener('click', linkToTwitter);
+    document.getElementById('twitterFavorite')
+        .addEventListener('click', linktoTwiterFavorite)
+    main();
+});*/
